54dd8dc611e5097e089819fad758dc0b7fdc7a75
jaremakulchyk <jaremakulchyk@gmail.com>
2023-10-17 12:09:27 +0200

    add prime columns

--- 100z.php
+++ 100z.php
@@ -16,10 +16,22 @@
         class Cell 
         {
             public $number;
+            public $isPrime;
+            public $isPartOfColumn = false;
+            public $class;
 
             function setNumber($number)
             {
                 $this->number = $number;
+            }
+            function setIsPrime($isPrime)
+            {
+                $this->isPrime = $isPrime;
+            }
+
+            function setIsPartOfColumn($isPartOfColumn)
+            {
+                $this->isPartOfColumn = $isPartOfColumn;
             }
 
             function getNumber()
@@ -27,15 +39,69 @@
                 return $this->number;
             }
 
-            function isPrime()
+        }
+
+
+        $matrix = initializeMatrix();
+
+        for($i = 0; $i < 10; $i++)
+        {
+            echo "<tr>";
+            for($j = 0; $j < 10; $j++)
+            {
+
+                if($matrix[$i][$j]->isPrime && !$matrix[$i][$j]->isPartOfColumn)
+                    {
+                        if($matrix[$i + 1][$j]->isPrime)
+                        {
+                            $matrix[$i][$j]->setIsPartOfColumn(true);
+                            $matrix[$i + 1][$j]->setIsPartOfColumn(true);
+                            if($matrix[$i + 2][$j]->isPrime)
+                            {
+                                $matrix[$i + 2][$j]->setIsPartOfColumn(true);
+                            }
+                        }
+                    }
+
+
+                if($matrix[$i][$j]->isPartOfColumn)
+                {
+                    $matrix[$i][$j]->class = "columnClass";
+                }
+                else if($matrix[$i][$j]->isPrime && !$matrix[$i][$j]->isPartOfColumn)
+                {
+                    $matrix[$i][$j]->class = "primeClass";
+                }
+                else
+                {
+                    $matrix[$i][$j]->class = "cell";
+                }
+                // echo '<p>number: ' .$matrix[$i][$j]->getNumber(). '</p>';
+                // echo '<p>part of column: ' .$matrix[$i][$j]->isPartOfColumn. '</p>';
+                // echo '<p>prime: ' .$matrix[$i][$j]->isPrime. '</p>';
+                // echo '<p>class: ' .$matrix[$i][$j]->class. '</p>';
+                // echo '<p>i: ' .$i. '</p>';
+                // echo '<p>j: ' .$j. '</p>';
+
+
+                echo '<td class="'.$matrix[$i][$j]->class.'">'
+                .$matrix[$i][$j]->getNumber().
+                '</td>';
+            }
+            echo "</tr>";
+            
+
+        }
+
+        function isPrime($number)
             {
                 $isPrime = true;
-                if($this->number == 1){
+                if($number == 1){
                     $isPrime = false;
                 }
-                for($i = 2; $i <= $this->number; $i++)
+                for($i = 2; $i <= $number; $i++)
                 {
-                    if(($this->number % $i == 0 && $i != $this->number))
+                    if(($number % $i == 0 && $i != $number))
                     {
                         //echo '<p>number:' .$number.', i:' .$i. '</p>';
                         $isPrime = false;
@@ -45,34 +111,22 @@
                 }
                 return $isPrime;
             }
-        }
 
-
-        $matrix = [];
-        
-        for($i = 0; $i < 10; $i++)
+        function initializeMatrix()
         {
-            $matrix[i] = [];
-            echo "<tr>";
-            for($j = 0; $j < 10; $j ++)
+            $matrix = [];
+            for ($i = 0; $i < 10; $i++)
             {
-                $matrix[$i][$j] = new Cell();
-                $matrix[$i][$j]->setNumber(($i * 10) + $j + 1);
-                if($matrix[$i][$j]->isPrime())
+                $matrix[$i] = [];
+                for ($j = 0; $j < 10; $j++)
                 {
-                    $class = "primeClass";
-                }
-                else 
-                {
-                    $class = "cell";
-                }
-                echo '<td class="'.$class.'">'
-                .$matrix[$i][$j]->getNumber().
-                '</td>';
-            }
-            echo "</tr>";
-            
+                    $matrix[$i][$j] = new Cell();
+                    $matrix[$i][$j]->setNumber(($i * 10) + $j + 1);
+                    $matrix[$i][$j]->setIsPrime(isPrime($matrix[$i][$j]->getNumber()));
 
+                }
+            }
+            return $matrix;
         }
 
         ?>
